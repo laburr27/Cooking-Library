ef0d81e0139644f8b21ff856d7baa2f8a3abdd2a
Lisa Anne Burr <burr_lisa@bah.com>
2017-02-28 14:18:45 -0500

    created controller files; tweaked views a little; working on creating CSS and HTML views

---
+++ app/controllers/bookshelves_controller.rb
@@ -0,0 +1,10 @@
+class BookshelvesController < ApplicationController
+
+  def index
+    @bookshelves = Bookshelf.all
+  end
+
+  def show
+    @bookshelf = Bookshelf.find(params[:id])
+  end
+end
---
+++ app/controllers/cookbooks_controller.rb
@@ -0,0 +1,34 @@
+class CookbooksController < ApplicationController
+
+  def index
+    @cookbooks = Cookbook.all
+  end
+
+  def new
+    @cookbook = Cookbook.new
+  end
+
+  def create
+    @cookbook = Cookbook.create(cookbook_params)
+    redirect_to cookbooks_path(@cookbook)
+  end
+
+  def show
+    @cookbook = Cookbook.find(params[:id])
+  end
+
+  def edit
+    @cookbook = Cookbook.find(params[:id])
+  end
+
+  def update
+    @cookbook = Cookbook.find(params[:id])
+    @cookbook.update(cookbook_params)
+    redirect_to cookbook_path(@cookbook)
+  end
+
+  private
+  def cookbook_params
+    params.require(:cookbook).permit(:title, :author, :img_url, :format, :summary, :bookshelf)
+  end
+end
