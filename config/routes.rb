c9f80fa18b99fe6dfed8b064515e42032a361be7
Lisa Anne Burr <burr_lisa@bah.com>
2017-03-01 23:38:26 -0500

    edit and new forms not working; updated @cookbook resources and ran new routes but no luck

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'bookshelves#index'
 
   resources :bookshelves, only: [:index, :show]
-  resources :cookbooks, only: [:index, :show, :new, :create, :edit]
+  resources :cookbooks, only: [:index, :new, :create, :show, :edit, :update]
 end
